2608317575cc5f16d4b9fc1f3e5247cf7fe728ac
Basmala-Abdullah <basmala.abdullah.ahmed@gmail.com>
2025-04-21 16:55:59 +0200

    feat: add receptionist role and related functionality for client management

---
+++ app/Http/Middleware/CheckReceptionistRole.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckReceptionistRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->user()->hasRole('receptionist')) {
+            abort(403);
+        }
+        return $next($request);
+    }
+}
