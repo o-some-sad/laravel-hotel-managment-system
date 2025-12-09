f4c2cce839ba3a3c0fc4ee88e34ebc2d1781bae5
moamen ayman <moamenayman596@gmail.com>
2025-12-09 13:49:38 +0200

    Fix Ziggy build error for Vercel

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,13 +4,14 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { Ziggy } from './ziggy';
 import Toast from "vue-toastification"
 import "vue-toastification/dist/index.css"
 
 // Theme initialization
-const theme = localStorage.getItem('theme') || 
-              (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+const theme = localStorage.getItem('theme') ||
+  (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 if (theme === 'dark') {
   document.documentElement.classList.add('dark');
 } else {
@@ -20,19 +21,19 @@ if (theme === 'dark') {
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
-        ),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .use(Toast)
-            .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => resolvePageComponent(
+    `./Pages/${name}.vue`,
+    import.meta.glob('./Pages/**/*.vue'),
+  ),
+  setup({ el, App, props, plugin }) {
+    return createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(ZiggyVue, Ziggy)
+      .use(Toast)
+      .mount(el);
+  },
+  progress: {
+    color: '#4B5563',
+  },
 });
